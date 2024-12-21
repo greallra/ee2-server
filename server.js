bf057403d3ce8ffa9ae5e5e913416320e5861cb2
greallra <greallra@tcd.ie>
2024-12-21 18:34:39 +0000

    env

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ import express from "express";
 import axios from "axios";
 import admin from "firebase-admin";
 import "./firebaseconfig.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import { getUsers, postNotication } from "./controllers/user.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -21,10 +25,28 @@ app.post("/notification", postNotication);
 
 import "./utils/crons.js";
 
-import serviceAccount from "/Users/ronangreally/Downloads/exchanges-b4fe1-firebase-adminsdk-mfrdv-4e24bcc3f3.json" assert { type: "json" };
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
+// import serviceAccount from "./ee2-server/exchanges-b4fe1-firebase-adminsdk-mfrdv-4e24bcc3f3.json" assert { type: "json" };
+console.log(12, process.env.API_KEY);
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: "service_account",
+    project_id: "exchanges-b4fe1",
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+
+    client_email:
+      "firebase-adminsdk-mfrdv@exchanges-b4fe1.iam.gserviceaccount.com",
+    client_id: "115384883805817604934",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url:
+      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-mfrdv%40exchanges-b4fe1.iam.gserviceaccount.com",
+    universe_domain: "googleapis.com",
+  }),
 });
 
 app.listen(port, () => {
